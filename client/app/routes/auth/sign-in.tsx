8ca8b40c316168a66143e7439f0effba851120ae
Akash Yadav <114010679+akash-lifegod@users.noreply.github.com>
2025-06-24 18:35:47 +0530

    sign-in-ui-1

--- client/app/routes/auth/sign-in.tsx
+++ client/app/routes/auth/sign-in.tsx
@@ -1,9 +1,86 @@
 import React, { use } from 'react'
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { signInSchema } from '@/lib/schema';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router';
 
+type signInFormData = z.infer<typeof signInSchema>;
 const SignIn = () => {
+  const form = useForm<signInFormData>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleOnSubmit = (values: signInFormData) => {
+    console.log('Form submitted:', values);
+  };
+
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-muted/40 p-4'>
+      <Card className='w-full max-w-md shadow-xl'>
+        <CardHeader className='text-center mb-5'>
+          <CardTitle className='text-2xl font-bold'>Welcome Back!</CardTitle>
+          <CardDescription className='text-sm text-muted-foreground'>
+            Sign in to your account to continue
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleOnSubmit)} className='space-y-6'>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl>
+                      <Input type ="email" {...field} placeholder="email@example.com"/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type ="password" {...field} placeholder="********"/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className='w-full'>
+                Sign In
+              </Button>
+            </form>
+          </Form>
+
+          <CardFooter className='flex items-center justify-center'>
+            <p className='text-sm text-muted-foreground'>
+              Don't have an account?
+              <Link to="/sign-up">
+                Sign Up
+              </Link>
+            </p>
+          </CardFooter>
+
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
