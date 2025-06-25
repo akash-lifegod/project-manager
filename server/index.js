983b3158245fef06eb7074299cafaabe8c6811da
Akash Yadav <114010679+akash-lifegod@users.noreply.github.com>
2025-06-25 17:44:55 +0530

    server-setup

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+app.use(morgan('dev'));
+
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('MongoDB connected successfully');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+});
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the TaskHub API',
+    });
+});
+
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        message: 'Internal Server Error',
+    });
+})
+
+// not found middleware
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Not Found',
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
