c8e69d6b9936044388447d0dda65ec195b32042d
Akash Yadav <114010679+akash-lifegod@users.noreply.github.com>
2025-06-25 16:23:25 +0530

    sign-up-schema

--- client/app/lib/schema.ts
+++ client/app/lib/schema.ts
@@ -5,3 +5,14 @@ export const signInSchema = z.object({
   password: z.string().min(6, 'Password is required'),
 });
 
+
+export const signUpSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 character long'),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters long'),
+  confirmPassword: z.string().min(8, 'Password must be at least 8 characters long'),
+})
+.refine((data) => data.password === data.confirmPassword, {
+  path: ['confirmPassword'],
+  message: "Passwords don't match",
+})
