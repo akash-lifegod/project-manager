abd3dccb48029171b546e98790ab6c1212b2e66e
Akash Yadav <114010679+akash-lifegod@users.noreply.github.com>
2025-07-21 23:20:55 +0530

    dashboard-header-work-in-progress

--- client/app/types/index.tsx
+++ client/app/types/index.tsx
@@ -3,4 +3,23 @@ export interface User {
     name: string;
     email: string;
     createdAt: string;
+    isEmailVerified: boolean;
+    updatedAt: string;
+    profilePicture?: string;
+}
+
+
+export interface Workspace {
+    _id: string;
+    name: string;
+    description?: string;
+    owner: User | string;
+    color: string;
+    members: {
+        user: User;
+        role: 'admin' | 'member' | 'owner' | 'viewer';
+        joinedAt: Date;
+    }[];
+    createdAt: Date;
+    upDatedAt: Date;
 }
